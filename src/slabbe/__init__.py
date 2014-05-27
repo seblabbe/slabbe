e325de2901a70eacf5524882b5728e1249506a2c
Sébastien Labbé <slabqc@gmail.com>
2014-05-27 23:13:26 +0200

    adding double_square_tile code from ticket #13069

--- src/slabbe/__init__.py
+++ src/slabbe/__init__.py
@@ -3,6 +3,15 @@ from billiard import BillardCube
 from discrete_plane import Plan, Line, DiscreteHyperplane
 from christoffel_graph import ChristoffelEdges
 from bispecial_extension_type import ExtensionType, ExtensionType1to1, ExtensionType2to1
+from double_square_tile import (DoubleSquare, 
+                                christoffel_tile,
+                                #find_square_factorisation,
+                                #double_square_from_boundary_word,
+                                #double_square_from_four_integers,
+                                #figure_11_BGL2012,
+                                #snake,
+                                #triple_square_example,
+                                )
 
 # for doctext to work, we import convex_boundary
 # from discrete_subset import convex_boundary
