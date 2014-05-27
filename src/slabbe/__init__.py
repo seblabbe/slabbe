a3bdbae5c92b38c65710d7db2a9c25434e5e7c2a
Sébastien Labbé <slabqc@gmail.com>
2014-05-27 13:11:56 +0200

    move files out of digital_geometry module

--- src/slabbe/__init__.py
+++ src/slabbe/__init__.py
@@ -1,2 +1,10 @@
-from slabbe.digital_geometry import *
+from discrete_subset import DiscreteSubset, DiscreteBox, DiscreteTube, Intersection
+from billiard import BillardCube
+from discrete_plane import Plan, Line, DiscreteHyperplane
+from christoffel_graph import ChristoffelEdges
 
+# for doctext to work, we import convex_boundary
+# from discrete_subset import convex_boundary
+
+# do not import module names just the above stuff
+#__all__ = []
