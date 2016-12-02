5568c24e255164f8e5a3c8b307fe0df5bcf60ded
Sébastien Labbé <slabqc@gmail.com>
2016-12-02 15:56:05 +0100

    ajout dim attribute to algos

--- slabbe/mult_cont_frac_pyx.pyx
+++ slabbe/mult_cont_frac_pyx.pyx
@@ -128,17 +128,19 @@ from sage.misc.prandom import random
 include "cysignals/signals.pxi"   # ctrl-c interrupt block support
 include "cysignals/memory.pxi"
 
+cdef double SQRT3SUR2 = 0.866025403784439
+
 cdef class PairPoint:
     cdef double* x
     cdef double* a
     cdef int dim
     cdef double _tmp
-    def __cinit__(self, dim, x=None, a=None):
+    def __cinit__(self, int dim, x=None, a=None):
         self.x = <double*>check_allocarray(dim, sizeof(double))
         self.a = <double*>check_allocarray(dim, sizeof(double))
         self.dim = dim
 
-    def __init__(self, d, x=None, a=None):
+    def __init__(self, int dim, x=None, a=None):
         cdef int i
         if x is None:
             for i in range(self.dim):
@@ -398,11 +400,12 @@ cdef class PairPoint:
         self.sort()
         return 100
 
-cdef int d = 3
-
-cdef double SQRT3SUR2 = 0.866025403784439
-
 cdef class MCFAlgorithm(object):
+    cdef int dim
+    def __cinit__(self, int dim=3):
+        self.dim = dim
+    #def __init__(self, int dim=3):
+    #    self.dim = dim
     ########################################
     # METHODS IMPLEMENTED IN HERITED CLASSES
     ########################################
@@ -465,7 +468,7 @@ cdef class MCFAlgorithm(object):
             {1, 2, 3, 123, 132, 213, 231, 312, 321}
         """
         cdef unsigned int i         # loop counter
-        cdef PairPoint P = PairPoint(d)
+        cdef PairPoint P = PairPoint(self.dim)
         cdef int branch
         S = set()
         # Loop
@@ -500,8 +503,8 @@ cdef class MCFAlgorithm(object):
         cdef double s,t             # temporary variables
         cdef unsigned int i         # loop counter
 
-        cdef PairPoint P = PairPoint(d)
-        cdef PairPoint R = PairPoint(d)
+        cdef PairPoint P = PairPoint(self.dim)
+        cdef PairPoint R = PairPoint(self.dim)
 
         cdef int branch
 
@@ -570,8 +573,8 @@ cdef class MCFAlgorithm(object):
         """
         from sage.modules.free_module_element import vector
         cdef unsigned int i         # loop counter
-        cdef PairPoint P = PairPoint(d)
-        cdef PairPoint R = PairPoint(d)
+        cdef PairPoint P = PairPoint(self.dim)
+        cdef PairPoint R = PairPoint(self.dim)
         cdef int branch
 
         A = dict((k,s.incidence_matrix()) for k,s in self.substitutions().iteritems())
@@ -659,6 +662,7 @@ cdef class MCFAlgorithm(object):
             "Arnoux-Rauzy-Poincar\\'e"
         """
         return self.class_name()
+
     def __repr__(self):
         r"""
         EXAMPLES::
@@ -669,7 +673,7 @@ cdef class MCFAlgorithm(object):
             sage: Brun()
             Brun 3-dimensional continued fraction algorithm
         """
-        return "{} 3-dimensional continued fraction algorithm".format(self.name())
+        return "{} {}-dimensional continued fraction algorithm".format(self.name(), self.dim)
 
     def __call__(self, PairPoint P):
         r"""
@@ -742,7 +746,7 @@ cdef class MCFAlgorithm(object):
             [123, 2, 1, 123, 1, 231, 3, 3, 3, 3, 123, 1, 1, 1, 231, 2, 321, 2, 3, 312]
 
         """
-        cdef PairPoint P = PairPoint(d, start)
+        cdef PairPoint P = PairPoint(self.dim, start)
         cdef int branch
 
         while True:
@@ -810,12 +814,12 @@ cdef class MCFAlgorithm(object):
 
         """
         cdef double s           # temporary variables
-        cdef PairPoint P = PairPoint(d)
+        cdef PairPoint P = PairPoint(self.dim)
         cdef int branch
         if start is None:
-            P = PairPoint(d, [random() for i in range(d)])
+            P = PairPoint(self.dim, [random() for i in range(self.dim)])
         else:
-            P = PairPoint(d, start)
+            P = PairPoint(self.dim, start)
         P.a[0] = 1./3
         P.a[1] = 1./3
         P.a[2] = 1./3
@@ -876,7 +880,7 @@ cdef class MCFAlgorithm(object):
 
         """
         cdef double s           # temporary variables
-        cdef PairPoint P = PairPoint(d)
+        cdef PairPoint P = PairPoint(self.dim)
         cdef int i
         cdef int branch
         if start is None:
@@ -998,7 +1002,7 @@ cdef class MCFAlgorithm(object):
         """
         cdef double s,x,y,u,v           # temporary variables
         s = x = y = u = v = 0           # initialize to avoid a warning
-        cdef PairPoint P = PairPoint(d)
+        cdef PairPoint P = PairPoint(self.dim)
         cdef int branch
         cdef int previous_branch
         cdef int xa,ya,ua,va
@@ -1106,7 +1110,7 @@ cdef class MCFAlgorithm(object):
             ((1.0, 0.0, 0.0), (59.0, 14.0, 4.0), 312)
         """
         cdef int branch
-        cdef PairPoint P = PairPoint(d)
+        cdef PairPoint P = PairPoint(self.dim)
         if start is None:
             P.x[0] = random(); P.x[1] = random(); P.x[2] = random()
         else:
@@ -1145,7 +1149,7 @@ cdef class MCFAlgorithm(object):
             Check for a fixed point stop then.
 
         """
-        cdef PairPoint P = PairPoint(d)
+        cdef PairPoint P = PairPoint(self.dim)
         cdef int branch, i
         if start is None:
             P.x[0] = random(); P.x[1] = random(); P.x[2] = random()
@@ -1189,7 +1193,7 @@ cdef class MCFAlgorithm(object):
             sage: Brun().image((10, 21, 37), 10)
             (1.0, 1.0, 0.0)
         """
-        cdef PairPoint P = PairPoint(d)
+        cdef PairPoint P = PairPoint(self.dim)
         cdef int i, branch
         P.x[0] = start[0]; P.x[1] = start[1]; P.x[2] = start[2]
         P.a[0] = 1
@@ -1274,7 +1278,8 @@ cdef class MCFAlgorithm(object):
             for i from 0 <= i <= ndivs:
                 C[i][j] = 0
 
-        cdef PairPoint P = PairPoint(d, [random() for i in range(d)], [0.3]*d)
+        cdef PairPoint P = PairPoint(self.dim, 
+                           [random() for i in range(self.dim)], [0.3]*self.dim)
         P.sort()
         P.normalize_x(p=norm)
 
@@ -1343,7 +1348,8 @@ cdef class MCFAlgorithm(object):
         cdef double u_new,v_new,w_new
         cdef int branch
 
-        cdef PairPoint P = PairPoint(d, [random() for i in range(d)], [1/3.]*d)
+        cdef PairPoint P = PairPoint(self.dim, 
+                           [random() for i in range(self.dim)], [1/3.]*self.dim)
         P.sort()
         P.normalize_x(1)
         cdef PairPoint R = P.copy()
@@ -1472,7 +1478,7 @@ cdef class MCFAlgorithm(object):
             s = x*u + y*v + z*w
             print("scal prod <(x,y,z),(u,v,w)> = %f" % s)
 
-        cdef PairPoint P = PairPoint(d)
+        cdef PairPoint P = PairPoint(self.dim)
         P.x[0] = x
         P.x[1] = y
         P.x[2] = z
@@ -1634,7 +1640,7 @@ cdef class Reverse(MCFAlgorithm):
             sage: Reverse()(P)
             ((0.55, 0.25, 0.04999999999999993), (0.6, 0.5, 0.5))
         """
-        cdef PairPoint R = PairPoint(d)
+        cdef PairPoint R = PairPoint(self.dim)
         if P.x[0] + P.x[1] < P.x[2]:
             P.x[2] -= P.x[0] + P.x[1]
             P.a[1] += P.a[2]
@@ -2392,7 +2398,7 @@ cdef class Sorted_ARrevert(MCFAlgorithm):
             sage: Sorted_ARrevert()(P)
             ((0.2, 0.3, 0.3), (0.3, 0.5, 0.6))
         """
-        cdef PairPoint R = PairPoint(d)
+        cdef PairPoint R = PairPoint(self.dim)
         cdef double z_x_y = P.x[2] - P.x[0] - P.x[1]
         if z_x_y > P.x[1]:
             R.x[0] = P.x[0]
@@ -2443,7 +2449,7 @@ cdef class Sorted_ARrevertMulti(MCFAlgorithm):
             sage: Sorted_ARrevertMulti()(P)
             ((0.20000000000000007, 0.3, 0.3), (0.3, 0.5, 0.6))
         """
-        cdef PairPoint R = PairPoint(d)
+        cdef PairPoint R = PairPoint(self.dim)
         cdef int m = <int>(P.x[2] / (P.x[0] + P.x[1]))
         cdef double z_mxy = P.x[2] - m * (P.x[0] + P.x[1])
         if m == 0:
@@ -2495,7 +2501,7 @@ cdef class Sorted_ARMonteil(MCFAlgorithm):
             sage: Sorted_ARMonteil()(P)         # tol
             ((0.2, 0.3, 0.3), (0.3, 0.5, 0.6))
         """
-        cdef PairPoint R = PairPoint(d)
+        cdef PairPoint R = PairPoint(self.dim)
 
         # Apply the algo
         if P.x[2] > P.x[0] + P.x[1]:
@@ -2525,7 +2531,7 @@ cdef class Sorted_Delaunay(MCFAlgorithm):
             sage: Sorted_Delaunay()(P)
             ((0.0, 0.3, 0.8), (0.6, 0.5, 0.3))
         """
-        cdef PairPoint R = PairPoint(d)
+        cdef PairPoint R = PairPoint(self.dim)
         # Apply the algo
         if P.x[2] > P.x[0] + P.x[1]:
             # Genre de semi revert
@@ -2585,7 +2591,7 @@ cdef class JacobiPerronAdditif(MCFAlgorithm):
             sage: JacobiPerronAdditif()(P)
             ((0.3, 0.3, 0.5), (0.5, 0.3, 0.3))
         """
-        cdef PairPoint R = PairPoint(d)
+        cdef PairPoint R = PairPoint(self.dim)
         # Apply the algo
         if P.x[0] < P.x[1]:
             R.x[0] = P.x[0]
@@ -2627,7 +2633,7 @@ cdef class JacobiPerronAdditifv2(MCFAlgorithm):
             sage: JacobiPerronAdditifv2()(P)
             ((0.3, 0.3, 0.5), (0.5, 0.3, 0.3))
         """
-        cdef PairPoint R = PairPoint(d)
+        cdef PairPoint R = PairPoint(self.dim)
         # Apply the algo
         if P.x[0] < P.x[1]:
             R.x[0] = P.x[0]
@@ -2658,97 +2664,6 @@ cdef class JacobiPerronAdditifv2(MCFAlgorithm):
             return 300
         else:
             raise ValueError("jacobi not defined for (x,y,z)=(%s,%s,%s)"%(P.x[0],P.x[1],P.x[2]))
-
-cdef class Modified(MCFAlgorithm):
-    r"""
-    EXAMPLES::
-
-        sage: from slabbe.mult_cont_frac_pyx import Modified, Brun, Reverse
-        sage: algo = Modified(Brun(), {123:1.2})
-        sage: algo
-        Modified 3-dimensional continued fraction algorithm
-
-    ::
-
-        sage: TestSuite(algo).run()
-        sage: algo._test_dual_substitution_definition()
-        sage: algo._test_coherence()          # not tested
-        sage: algo._test_definition()
-
-    For example::
-
-        sage: f = lambda g: Modified(Brun(), {123:g}).lyapunov_exponents(n_iterations=1000000)[-1]
-        sage: plot(f, (0.95, 1.05), adaptive_recursion=0)   # not tested
-        Graphics object consisting of 1 graphics primitive
-
-
-    The motivation for this class is to study the constant in front of the not
-    unimodular matrix for branch 4 in Reverse algorithm::
-
-        sage: algo = Modified(Reverse(), {4:1})
-        sage: algo.lyapunov_exponents(n_iterations=1000000)    # abs tol 0.1
-        (0.407428273374779, -0.10394067692044444, 1.2551140500375464)
-        sage: algo = Modified(Reverse(), {4:2.^(1/3)})       # unimodular case
-        sage: algo.lyapunov_exponents(n_iterations=1000000)    # abs tol 0.1
-        (0.3629971890178995, -0.14441058920752092, 1.397828395305838)
-
-    Question: what is the 1-theta2/theta1 de l'algo Reverse?
-
-    More systematically::
-
-        sage: f = lambda g: Modified(Reverse(), {4:g}).lyapunov_exponents(n_iterations=1000000)[-1]
-        sage: plot(f, (0.95, 1.05), adaptive_recursion=0)  # not tested
-        Graphics object consisting of 1 graphics primitive
-    """
-    cdef MCFAlgorithm _algo
-    cdef dict _gamma
-    def __init__(self, MCFAlgorithm, dict gamma):
-        self._algo = MCFAlgorithm
-        self._gamma = gamma
-
-    cdef int call(self, PairPoint P) except *:
-        r"""
-        EXAMPLES::
-
-            sage: from slabbe.mult_cont_frac_pyx import Modified, Brun
-            sage: from slabbe.mult_cont_frac_pyx import PairPoint
-            sage: P = PairPoint(3, [.3,.6,.8], [.2,.3,.3])
-            sage: algo = Modified(Brun(), {132:1})
-            sage: algo(P)
-            ((0.3, 0.6, 0.20000000000000007), (0.2, 0.6, 0.3))
-
-        ::
-
-            sage: P = PairPoint(3, [.3,.6,.8], [.2,.3,.3])
-            sage: algo = Modified(Brun(), {123:2})
-            sage: algo(P)
-            ((0.6, 1.2, 0.40000000000000013), (0.1, 0.3, 0.15))
-        """
-        cdef int branch
-        branch = self._algo.call(P)
-        if branch in self._gamma:
-            g = self._gamma[branch]
-            P.x[0] *= g
-            P.x[1] *= g
-            P.x[2] *= g
-            P.a[0] /= g
-            P.a[1] /= g
-            P.a[2] /= g
-        return branch
-
-    def substitutions(self):
-        r"""
-        EXAMPLES::
-
-        """
-        return self._algo.substitutions()
-
-    def dual_substitutions(self):
-        r"""
-        EXAMPLES::
-
-        """
-        return self._algo.dual_substitutions()
 
 cdef inline (double, double) projection3to2(double x, double y, double z):
     cdef double s = -SQRT3SUR2 * x + SQRT3SUR2 * y
