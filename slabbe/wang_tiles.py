2444494e622e254e864296a50bcd1b8a9af648ed
Sébastien Labbé <slabqc@gmail.com>
2017-05-23 13:53:52 +0200

    a class for wang tiling is better

--- slabbe/wang_tiles.py
+++ slabbe/wang_tiles.py
@@ -10,20 +10,22 @@ EXAMPLES::
 
     sage: tiles = [(0,0,0,0), (1,1,1,1), (2,2,2,2)]
     sage: W = WangTileSolver(tiles,3,4)
-    sage: _ = W.tikz().pdf()
+    sage: tiling = W.solve()
+    sage: _ = tiling.tikz().pdf()
 
 ::
 
     sage: tiles = [(1/2,1/2,1/2,1/2), (1,1,1,1), (2,2,2,2)]
     sage: W = WangTileSolver(tiles,3,4)
-    sage: _ = W.tikz().pdf()
+    sage: tiling = W.solve()
+    sage: _ = tiling.tikz().pdf()
 
 ::
 
     sage: tiles = [(0,3,1,4), (1,4,0,3)]
     sage: W = WangTileSolver(tiles,3,4)
-    sage: table = W.solve()
-    sage: table
+    sage: tiling = W.solve()
+    sage: tiling._table
     [[0, 1, 0, 1], [1, 0, 1, 0], [0, 1, 0, 1]]
 
 Kari-Culik (Here 0' is replaced by 10)::
@@ -34,14 +36,16 @@ Kari-Culik (Here 0' is replaced by 10)::
     ....:     (1,1,2,0)]
     sage: tiles = divide_by_2 + times_3
     sage: W = WangTileSolver(tiles,3,4)
-    sage: _ = W.tikz().pdf()
+    sage: tiling = W.solve()
+    sage: _ = tiling.tikz().pdf()
 
 Rao-Jeandel::
 
     sage: tiles = [(2,4,2,1), (2,2,2,0), (1,1,3,1), (1,2,3,2), (3,1,3,3),
     ....: (0,1,3,1), (0,0,0,1), (3,1,0,2), (0,2,1,2), (1,2,1,4), (3,3,1,2)]
     sage: W = WangTileSolver(tiles,3,4)
-    sage: _ = W.tikz().pdf()
+    sage: tiling = W.solve()
+    sage: _ = tiling.tikz().pdf()
 
 .. TODO::
 
@@ -77,7 +81,8 @@ class WangTileSolver(object):
 
         sage: tiles = [(0,0,0,0), (1,1,1,1), (2,2,2,2)]
         sage: W = WangTileSolver(tiles, 3, 3)
-        sage: W.solve()
+        sage: tiling = W.solve()
+        sage: tiling._table
         [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
     With color 2 preassigned to the right part of tile at position (1,1)::
@@ -85,7 +90,8 @@ class WangTileSolver(object):
         sage: tiles = [(0,0,0,0), (1,1,1,1), (2,2,2,2)]
         sage: right = {(1,1):2}
         sage: W = WangTileSolver(tiles,3,3,preassigned=[right,{},{},{}])
-        sage: W.solve()
+        sage: tiling = W.solve()
+        sage: tiling._table
         [[2, 2, 2], [2, 2, 2], [2, 2, 2]]
 
     When constraints are inconsistent::
@@ -185,7 +191,8 @@ class WangTileSolver(object):
 
             sage: tiles = [(0,3,1,4), (1,4,0,3)]
             sage: W = WangTileSolver(tiles,3,4)
-            sage: table = W.solve()
+            sage: tiling = W.solve()
+            sage: table = tiling._table
             sage: table
             [[0, 1, 0, 1], [1, 0, 1, 0], [0, 1, 0, 1]]
 
@@ -202,7 +209,66 @@ class WangTileSolver(object):
         table = [[0]*self._height for _ in range(self._width)]
         for i,j,k in support:
             table[j][k] = i
-        return table
+        return WangTiling(table, self._tiles, color=self._color)
+
+class WangTiling(object):
+    r"""
+    INPUT:
+
+    - ``table`` -- list of lists
+    - ``tiles`` -- list of tiles, a tile is a 4-tuple (right color, top
+        color, left color, bottom color)
+    - ``color`` -- dict (default: None)
+
+    EXAMPLES::
+
+        sage: tiles = [(0,3,1,4), (1,4,0,3)]
+        sage: table = [[0, 1, 0, 1], [1, 0, 1, 0], [0, 1, 0, 1]]
+        sage: tiling = WangTiling(table, tiles)
+        sage: tiling
+        A wang tiling of a 3 x 4 rectangle
+    """
+    def __init__(self, table, tiles, color=None):
+        r"""
+        See class for documentation.
+
+        EXAMPLES::
+
+            sage: tiles = [(0,0,0,0), (1,1,1,1), (2,2,2,2)]
+            sage: W = WangTileSolver(tiles, 3, 4)
+            sage: W.solve()
+            A wang tiling of a 3 x 4 rectangle
+        """
+        self._tiles = tiles
+        self._table = table
+        self._color = color
+
+    def __repr__(self):
+        return "A wang tiling of a {} x {} rectangle".format(self.width(), self.height())
+
+    def height(self):
+        r"""
+        EXAMPLES::
+
+            sage: tiles = [(0,3,1,4), (1,4,0,3)]
+            sage: table = [[0, 1, 0, 1], [1, 0, 1, 0], [0, 1, 0, 1]]
+            sage: tiling = WangTiling(table, tiles)
+            sage: tiling.height()
+            4
+        """
+        return len(table[0])
+
+    def width(self):
+        r"""
+        EXAMPLES::
+
+            sage: tiles = [(0,3,1,4), (1,4,0,3)]
+            sage: table = [[0, 1, 0, 1], [1, 0, 1, 0], [0, 1, 0, 1]]
+            sage: tiling = WangTiling(table, tiles)
+            sage: tiling.width()
+            3
+        """
+        return len(table)
 
     def horizontal_words_list(self, side=3):
         r"""
@@ -217,15 +283,15 @@ class WangTileSolver(object):
 
             sage: tiles = [(0,3,1,4), (1,4,0,3)]
             sage: W = WangTileSolver(tiles,3,4)
-            sage: W.horizontal_words_list()
+            sage: tiling = W.solve()
+            sage: tiling.horizontal_words_list()
             [[4, 3, 4], [3, 4, 3], [4, 3, 4], [3, 4, 3]]
-            sage: W.horizontal_words_list(0)
+            sage: tiling.horizontal_words_list(0)
             [[0, 1, 0], [1, 0, 1], [0, 1, 0], [1, 0, 1]]
         """
-        table = self.solve()
         rep = []
-        for i in range(self._height):
-            row = [table[j][i] for j in range(self._width)]
+        for i in range(self.height()):
+            row = [self._table[j][i] for j in range(self.width())]
             L = [self._tiles[r][side] for r in row]
             rep.append(L)
         return rep
@@ -242,7 +308,9 @@ class WangTileSolver(object):
 
             sage: tiles = [(0,0,0,0), (1,1,1,1), (2,2,2,2)]
             sage: W = WangTileSolver(tiles,3,4)
-            sage: t = W.tikz()
+            sage: tiling = W.solve()
+            sage: t = tiling.tikz()
+            sage: t
             \documentclass[tikz]{standalone}
             \usepackage{amsmath}
             \begin{document}
@@ -252,7 +320,7 @@ class WangTileSolver(object):
             \draw (0, 0) -- (0, 1);
             \node[left]  at (1, 0.5) {0};
             ...
-            ... 76 lines not printed (2353 characters in total) ...
+            ... 76 lines not printed (2365 characters in total) ...
             ...
             \node[left]  at (3, 3.5) {0};
             \node[below] at (2.5, 4) {0};
@@ -267,23 +335,24 @@ class WangTileSolver(object):
             sage: tiles = [(0,2,1,3), (1,3,0,2)]
             sage: color = {0:'white',1:'red',2:'blue',3:'green'}
             sage: W = WangTileSolver(tiles,3,4,color=color)
-            sage: t = W.tikz()
+            sage: tiling = W.solve()
+            sage: t = tiling.tikz()
 
         With colors, alternatively::
 
             sage: tiles = [(0,2,1,3), (1,3,0,2)]
             sage: W = WangTileSolver(tiles,3,4)
+            sage: tiling = W.solve()
             sage: color = {0:'white',1:'red',2:'blue',3:'green'}
-            sage: t = W.tikz(color=color)
+            sage: t = tiling.tikz(color=color)
         """
-        table = self.solve()
         if color is None:
             color = self._color
         lines = []
         lines.append(r'\begin{tikzpicture}')
-        for j in range(self._width):
-            for k in range(self._height):
-                i = table[j][k]
+        for j in range(self.width()):
+            for k in range(self.height()):
+                i = self._table[j][k]
                 tile = self._tiles[i]
                 lines.append('% tile at position {}'.format((j,k)))
                 if color:
