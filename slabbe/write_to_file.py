8b4653a3319ace0782f11b4a61831b28a637407a
Sébastien Labbé <slabqc@gmail.com>
2018-11-30 17:25:37 +0100

    ajout print set to file method

--- slabbe/write_to_file.py
+++ slabbe/write_to_file.py
@@ -41,3 +41,37 @@ def write_str_to_file(s, filename):
     with open(filename, 'w') as f:
         print("Creation of {}".format(filename))
         f.write(s)
+
+def print_set_to_file(s, filename, ncolumns=4, start=r'\{', end=r'\}',
+        sep=r',', eol=r',\\&\hspace{10mm}'):
+    r"""
+    INPUT:
+
+    - ``s`` -- string
+    - ``filename`` -- string, filename
+
+    EXAMPLES::
+
+        sage: from slabbe.write_to_file import print_set_to_file
+        sage: filename = tmp_filename(ext='.tex')
+        sage: M = [random_matrix(ZZ, 3, 3) for _ in range(20)]
+        sage: print_set_to_file(M, filename, ncolumns=8, 
+        ....:          start=r'\left\{\begin{array}{cccccccc}',
+        ....:          end=r'\end{array}\right\}',
+        ....:          sep=r',&', eol=r',\\[4mm]')
+        Creation of ...tex
+
+    """
+    from sage.misc.latex import latex
+    L = [start]
+    for i,a in enumerate(s):
+        L.append(latex(a))
+        if (i+1) == len(s):
+            L.append(end)
+        elif (i+1) % ncolumns == 0:
+            L.append(eol)
+        else:
+            L.append(sep)
+    output = '\n'.join(L)
+    write_str_to_file(output, filename)
+
